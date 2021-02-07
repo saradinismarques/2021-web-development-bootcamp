3b14757099119f68b34fcea1f1384955b28f3ba5
saradinismarques <sarr.filipadinis@gmail.com>
2021-02-07 00:19:43 +0000

    adding pages

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -1,9 +1,66 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+//For this we need to install the npm module @mailchimp/mailchimp_marketing. To do that we write:
+//npm install @mailchimp/mailchimp_marketing
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
-app.listen(3000, function() {
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+//Setting up MailChimp
+mailchimp.setConfig({
+     apiKey: "fe9a915ed223ea88c93c4731693bf4a8-us7",
+     server: "us7"
+});
+
+app.post("/", function (req,res) {
+
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const listId = "d6dbd064bf";
+
+    //Creating an object with the users data
+    const subscribingUser = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email
+    };
+    //Uploading the data to the server
+    async function run() {
+        const response = await mailchimp.lists.addListMember(listId, {
+            email_address: subscribingUser.email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: subscribingUser.firstName,
+                LNAME: subscribingUser.lastName
+            }
+        });
+        //If all goes well logging the contact's id
+        res.sendFile(__dirname + "/success.html");
+
+        console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`);
+    };
+
+    //Running the function and catching the errors (if any)
+    // So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
+    run().catch(e => res.sendFile(__dirname + "/failure.html"));
+});
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server is running on port 3000.")
 });
