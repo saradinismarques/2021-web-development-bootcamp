df6f1fe134ef67bdc365f7a4aa63c19d70ede65c
saradinismarques <sarr.filipadinis@gmail.com>
2021-02-24 18:42:04 +0000

    finish react module

---
+++ react-module/registration-screen/forms/src/components/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function App() {
+
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleClick(e) {
+    setHeading(name);
+
+    e.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      {/* <h1>Hello {name}</h1> */}
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input 
+          onChange={handleChange}
+          type="text" 
+          placeholder="What's your name?" 
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
